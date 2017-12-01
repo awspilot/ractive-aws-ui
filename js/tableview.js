029126a02c113e89db3ff3aa069b68c60a639e6c
databank <adrian@databank.ro>
2017-12-01 21:04:32 +0200

    tableview

--- js/tableview.js
+++ js/tableview.js
@@ -16,7 +16,68 @@ Ractive.components.tableview = Ractive.extend({
 Ractive.components.tablebrowse = Ractive.extend({
 	template: "\
 		<div class='tablebrowse'>\
-			browse table {{.table.name}}\
+			<!-- table query -->\
+			<tabledata columns='{{columns}}' rows='{{rows}}'/>\
+		</div>",
+	data: {},
+	oninit: function() {
+		var ractive = this;
+		this.set('columns', [])
+		this.set('rows', [])
+
+		var params = {
+			TableName: this.get('table.name')
+		};
+		ddb.scan(params, function(err, data) {
+			var fields = {}
+			var columns = []
+			var rows = []
+
+			if (err)
+				return;
+
+			data.Items.map(function(row) {
+				Object.keys(row).map(function(column_name) {
+					if (!fields.hasOwnProperty(column_name)) {
+						fields[column_name] = {};
+						columns.push(column_name)
+
+					}
+				})
+			})
+			data.Items.map(function(row) {
+				var thisrow = []
+				columns.map(function(column_name) {
+					thisrow.push(row[column_name])
+				})
+				rows.push(thisrow)
+			})
+			ractive.set('columns', columns )
+			ractive.set('rows', rows )
+
+		})
+	},
+})
+Ractive.components.tabledata = Ractive.extend({
+	template: "\
+		<div class='tabledata'>\
+			<div class='tabledatahead'>\
+				{{#columns}}\
+					<div style='width: {{100/columns.length}}%'>{{.}}</div>\
+				{{/columns}}\
+			</div>\
+			<div class='tabledatacontent'>\
+				{{#rows}}\
+				<div class='tabledatarow'>\
+					{{#each .}}\
+					<div class='tabledatacell' style='width: {{100/columns.length}}%'>\
+						{{#if .S}}{{.S}}{{/if}}\
+						{{#if .N}}{{.N}}{{/if}}\
+					</div>\
+					{{/each}}\
+				</div>\
+				{{/rows}}\
+			</div>\
 		</div>",
 	data: {},
 	oninit: function() {
