8b65b5aa6d6e2b7f237500ffb272f643209fcf7d
databank <adrian@databank.ro>
2017-12-09 14:43:41 +0200

    localStorage for AWS credentials

---
+++ js/session.js
@@ -0,0 +1,31 @@
+
+var $session
+(function() {
+	function session() {
+	}
+	session.prototype.account_list = function() {
+		var accounts = JSON.parse(localStorage.getItem("accounts") || "[]")
+
+		return accounts
+	}
+
+	session.prototype.account_add = function( name, key ) {
+
+		var new_account = {
+			id: id=Math.random().toString().slice(2),
+			name: name,
+			key: key,
+		}
+		var account_list = this.account_list();
+		account_list.push( new_account )
+
+		localStorage.setItem("accounts", JSON.stringify(account_list))
+
+	}
+	session.prototype.account_delete = function( account ) {
+		var account_list = this.account_list().filter(function(a) { return a.id !== account.id })
+		localStorage.setItem("accounts", JSON.stringify(account_list))
+	}
+
+	$session = new session()
+})($session)
