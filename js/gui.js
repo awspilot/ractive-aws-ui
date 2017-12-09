9ebfeb3dbe71cf366a9d84d9fead05a266980f24
databank <adrian@databank.ro>
2017-12-09 14:49:31 +0200

    rebuild ui with login

--- js/gui.js
+++ js/gui.js
@@ -1,30 +1,37 @@
+
 var ractive;
+var selected_account;
 window.addEventListener('load', function() {
-	var r = new Ractive({
+	ractive = new Ractive({
 		el: 'body',
 		template: "\
-			<header></header>\
-			<left>\
-				<tablelist />\
-			</left>\
-			<content>\
-				<tabs />\
-			</content>",
+			{{#if selected_account}}\
+				<dynamoui account='{{selected_account}}' />\
+			{{else}}\
+				<login />\
+			{{/if}}\
+			",
 		data: {},
 	})
-	ractive = r
+
 	ractive.on('open-table', function(e, table ) {
 		ractive.findComponent('tabs').newtab('tableview', table )
 	})
+	ractive.on('login.switch-account', function(e) {
+		selected_account = ractive.findComponent('login').get(e.resolve())
+		ractive.set('selected_account', selected_account )
+		return false;
+	})
+	ractive.on('login.delete-account', function(e) {
+		$session.account_delete(ractive.findComponent('login').get(e.resolve()))
+		ractive.findComponent('login').set('accounts', $session.account_list() )
+		return false;
+	})
+	ractive.on('login.add-account', function(e) {
+		var new_account = ractive.findComponent('login').get('new');
+		$session.account_add(new_account.name, new_account.key )
+		ractive.findComponent('login').set('accounts', $session.account_list() )
+		ractive.findComponent('login').set('new')
+		return false;
+	})
 })
-
-
-var ddb = new AWS.DynamoDB({
-	region: 'us-east-1',
-	credentials: {
-		accessKeyId: 'xxxxxxx',
-		secretAccessKey: 'yyyyyyy',
-	}
-})
-//var DynamoDB = new window['@awspilot/dynamodb'](ddb)
-var DynamoSQL = new window['@awspilot/dynamodb-sql'](ddb)
