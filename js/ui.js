2abb106fca4e919971fb88681adbdf51167a590b
adrianpraja <adrian@databank.ro>
2018-09-09 19:37:31 +0300

    update

--- js/ui.js
+++ js/ui.js
@@ -22,7 +22,7 @@ Ractive.components.dynamoui = Ractive.extend({
 
 		if (this.get('account.endpoint')) {
 			credentials.endpoint = this.get('account.endpoint')
-			if ((location.protocol + '//' + location.host).indexOf(this.get('account.endpoint')) === 0) {
+			if (this.get('account.endpoint').indexOf( location.protocol + '//' + location.host ) === 0) {
 				// dynamodb is proxied via same host, force version signature 3 so Authorization header is not used
 				credentials.signatureVersion = 'v3'
 				// httpOptions: { xhrWithCredentials: true },
