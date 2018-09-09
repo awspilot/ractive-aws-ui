7ae194ce9a4b0ddc0ff9d54eef6d66239b9f76de
adrianpraja <adrian@databank.ro>
2018-09-09 19:32:29 +0300

    update

--- js/ui.js
+++ js/ui.js
@@ -19,8 +19,15 @@ Ractive.components.dynamoui = Ractive.extend({
 	},
 	oninit: function() {
 		var credentials = this.get('account.key')
-		if (this.get('account.endpoint'))
+
+		if (this.get('account.endpoint')) {
 			credentials.endpoint = this.get('account.endpoint')
+			if ((location.protocol + '//' + location.host).indexOf(this.get('account.endpoint')) === 0) {
+				// dynamodb is proxied via same host, force version signature 3 so Authorization header is not used
+				credentials.signatureVersion = 'v3'
+				// httpOptions: { xhrWithCredentials: true },
+			}
+		}
 
 		ddb = new AWS.DynamoDB(credentials)
 		DynamoSQL = new window['@awspilot/dynamodb-sql'](ddb)
