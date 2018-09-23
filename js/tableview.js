1f55b39591ee51769b15f7fc66cc13c39ebc485c
adrianpraja <adrian@databank.ro>
2018-09-23 12:05:14 +0300

    update

--- js/tableview.js
+++ js/tableview.js
@@ -152,45 +152,8 @@ Ractive.components.tabledata = Ractive.extend({
 			<div class='btn btn-xs pull-right' on-click='filter'><i class='zmdi zmdi-filter-list'></i></div>\
 			<div class='btn btn-xs pull-right' on-click='refresh'><i class='zmdi zmdi-refresh'></i></div>\
 		</div>\
-		<div class='tabledata'>\
-			<div class='tabledatahead'>\
-				{{#columns:i}}\
-					<div style='width: {{#if i === 0}}22px{{else}}{{100/columns.length}}%{{/if}} '>{{.}}</div>\
-				{{/columns}}\
-			</div>\
-			<div class='tabledatacontent'>\
-				{{#rows:row}}\
-				<div class='tabledatarow {{#if .[0].selected}}selected{{/if}}' on-click='selectrow'>\
-					{{#each .:i}}\
-					<div class='tabledatacell\
-						{{#if .KEY}}t-K{{/if}}\
-						{{#if .S}}t-S{{/if}}\
-						{{#if .N}}t-N{{/if}}\
-						{{#if .BOOL}}t-BOOL{{/if}}\
-						{{#if .NULL}}t-NULL{{/if}}\
-						{{#if .L}}t-L{{/if}}\
-						{{#if .M}}t-M{{/if}}\
-						{{#if .U}}t-U{{/if}}\
-						' style='width: {{#if i === 0}}22px{{else}}{{100/columns.length}}%{{/if}} '>\
-						{{#if .KEY}}\
-							{{#if .selected}}\
-								<i class='zmdi selectrow zmdi-hc-fw zmdi-check-square'></i>\
-							{{else}}\
-								<i class='zmdi selectrow zmdi-hc-fw zmdi-square-o'></i>\
-							{{/if}}\
-						{{/if}}\
-						{{#if .S}}{{.S}}{{/if}}\
-						{{#if .N}}{{.N}}{{/if}}\
-						{{#if .BOOL}}{{.BOOL}}{{/if}}\
-						{{#if .NULL}}NULL{{/if}}\
-						{{#if .L}}[...]{{/if}}\
-						{{#if .M}}{...}{{/if}}\
-					</div>\
-					{{/each}}\
-				</div>\
-				{{/rows}}\
-			</div>\
-		</div>",
+		<tabledata columns='{{columns}}' rows='{{rows}}' />\
+		",
 	data: {},
 	oninit: function() {
 		var ractive = this
