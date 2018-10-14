bf19f245348432b484cc7c1ac8a47250d8851a38
adrianpraja <adrian@databank.ro>
2018-10-14 11:32:14 +0300

    info tab

--- js/tableview.js
+++ js/tableview.js
@@ -14,8 +14,12 @@ Ractive.components.tableview = Ractive.extend({
 				<a class='btn-tableview-tab {{#if tab === \"triggers\"}}active{{/if}}'     on-click='@this.set(\"tab\",\"triggers\")'><i class='zmdi zmdi-portable-wifi'></i></a>\
 			</div>\
 			<div style='position: absolute;top: 0px;left: 40px;right: 0px;bottom: 0px;'>\
+				{{#if tab === 'info'}}\
+					<tableinfo table='{{.table}}'/>\
+				{{/if}}\
+				\
 				{{#if tab === 'data'}}\
-				<tablebrowse table='{{.table}}'/>\
+					<tablebrowse table='{{.table}}'/>\
 				{{/if}}\
 			</div>\
 		</div>",
@@ -237,5 +241,138 @@ Ractive.components.tablebrowsehead = Ractive.extend({
 			})
 
 		})
+	},
+})
+
+Ractive.components.tableinfo = Ractive.extend({
+	template: "\
+		<div class='tableinfo'>\
+			<scrollarea class='scrollarea' style='position: absolute;top: 0px;left: 0px;bottom: 0px;right: 0px;'>\
+				<div style='padding: 30px'>\
+					<h3>Table details</h3>\
+					<br>\
+					<div style='color:red'>{{ err }}</div>\
+					<hr>\
+					<table>\
+						<tr>\
+							<td align='right' width='350'><b>Table name</b></td>\
+							<td> {{ describeTable.TableName }}</td>\
+						</tr>\
+						<tr>\
+							<td align='right'><b>Primary partition key</b></td>\
+							<td>\
+								{{#describeTable.KeySchema:i}}\
+									{{#if .KeyType === 'HASH'}}\
+										{{.AttributeName}}\
+										{{# ~/describeTable.AttributeDefinitions }}\
+											{{#if .AttributeName === ~/.describeTable.KeySchema[i].AttributeName }}\
+												{{#if .AttributeType === 'S'}}\
+													( String )\
+												{{/if}}\
+												{{#if .AttributeType === 'N'}}\
+													( Number )\
+												{{/if}}\
+												{{#if .AttributeType === 'B'}}\
+													( Binary )\
+												{{/if}}\
+											{{/if}}\
+										{{/}}\
+									{{/if}}\
+								{{/describeTable.KeySchema}}\
+							</td>\
+						</tr>\
+						<tr>\
+							<td align='right'><b>Primary sort key</b></td>\
+							<td>\
+								{{#describeTable.KeySchema:i}}\
+									{{#if .KeyType === 'RANGE'}}\
+										{{.AttributeName}}\
+										{{# ~/describeTable.AttributeDefinitions }}\
+											{{#if .AttributeName === ~/.describeTable.KeySchema[i].AttributeName }}\
+												{{#if .AttributeType === 'S'}}\
+													( String )\
+												{{/if}}\
+												{{#if .AttributeType === 'N'}}\
+													( Number )\
+												{{/if}}\
+												{{#if .AttributeType === 'B'}}\
+													( Binary )\
+												{{/if}}\
+											{{/if}}\
+										{{/}}\
+									{{/if}}\
+								{{/describeTable.KeySchema}}\
+							</td>\
+						</tr>\
+						<tr>\
+							<td align='right'><b>Point-in-time recovery</b></td>\
+							<td></td>\
+						</tr>\
+						<tr>\
+							<td align='right'><b>Encryption</b></td>\
+							<td></td>\
+						</tr>\
+						<tr>\
+							<td align='right'><b>Time to live attribute</b></td>\
+							<td></td>\
+						</tr>\
+						<tr>\
+							<td align='right'><b>Table status	</b></td>\
+							<td></td>\
+						</tr>\
+						<tr>\
+							<td align='right'><b>Creation date</b></td>\
+							<td></td>\
+						</tr>\
+						<tr>\
+							<td align='right'><b>Provisioned read capacity units</b></td>\
+							<td></td>\
+						</tr>\
+						<tr>\
+							<td align='right'><b>Provisioned write capacity units</b></td>\
+							<td></td>\
+						</tr>\
+						<tr>\
+							<td align='right'><b>Last decrease time</b></td>\
+							<td></td>\
+						</tr>\
+						<tr>\
+							<td align='right'><b>Last increase time</b></td>\
+							<td></td>\
+						</tr>\
+						<tr>\
+							<td align='right'><b>Storage size (in bytes)	</b></td>\
+							<td></td>\
+						</tr>\
+						<tr>\
+							<td align='right'><b>Item count</b></td>\
+							<td></td>\
+						</tr>\
+						<tr>\
+							<td align='right'><b>Region</b></td>\
+							<td></td>\
+						</tr>\
+						<tr>\
+							<td align='right'><b>Amazon Resource Name (ARN)</b></td>\
+							<td></td>\
+						</tr>\
+					</table>\
+					<small>Storage size and item count are not updated in real-time. They are updated periodically, roughly every six hours.</small>\
+				</div>\
+			</scrollarea>\
+		</div>",
+	data: function() { return {} },
+	oninit: function() {
+		var ractive = this;
+
+		routeCall({ method: 'describeTable', payload: { TableName: ractive.get('table.name')} }, function(err, data) {
+			if (err)
+				return ractive.set('err', err.errorMessage );
+
+			console.log(data)
+			ractive.set('describeTable', data.Table)
+		})
+
+
 	},
 })
