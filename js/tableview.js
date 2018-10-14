00c4beb7f459099670d954d1fc225abe53ec039a
adrianpraja <adrian@databank.ro>
2018-10-14 12:06:13 +0300

    info tab

--- js/tableview.js
+++ js/tableview.js
@@ -249,11 +249,17 @@ Ractive.components.tableinfo = Ractive.extend({
 		<div class='tableinfo'>\
 			<scrollarea class='scrollarea' style='position: absolute;top: 0px;left: 0px;bottom: 0px;right: 0px;'>\
 				<div style='padding: 30px'>\
-					<h3>Table details</h3>\
-					<br>\
+					<h3>\
+						Table details\
+						<a class='btn btn-xs pull-right' on-click='refresh-table'><i class='icon zmdi zmdi-refresh'></i></a>\
+					</h3>\
 					<div style='color:red'>{{ err }}</div>\
 					<hr>\
 					<table>\
+						<tr>\
+							<td align='right' width='350'><b>Table ID</b></td>\
+							<td> {{ describeTable.TableId }}</td>\
+						</tr>\
 						<tr>\
 							<td align='right' width='350'><b>Table name</b></td>\
 							<td> {{ describeTable.TableName }}</td>\
@@ -317,36 +323,36 @@ Ractive.components.tableinfo = Ractive.extend({
 							<td></td>\
 						</tr>\
 						<tr>\
-							<td align='right'><b>Table status	</b></td>\
-							<td></td>\
+							<td align='right'><b>Table status</b></td>\
+							<td>{{describeTable.TableStatus}}</td>\
 						</tr>\
 						<tr>\
 							<td align='right'><b>Creation date</b></td>\
-							<td></td>\
+							<td>{{describeTable.CreationDateTime}}</td>\
 						</tr>\
 						<tr>\
 							<td align='right'><b>Provisioned read capacity units</b></td>\
-							<td></td>\
+							<td>{{describeTable.ProvisionedThroughput.ReadCapacityUnits}}</td>\
 						</tr>\
 						<tr>\
 							<td align='right'><b>Provisioned write capacity units</b></td>\
-							<td></td>\
+							<td>{{describeTable.ProvisionedThroughput.WriteCapacityUnits}}</td>\
 						</tr>\
 						<tr>\
 							<td align='right'><b>Last decrease time</b></td>\
-							<td></td>\
+							<td>{{describeTable.ProvisionedThroughput.LastDecreaseDateTime || '-' }}</td>\
 						</tr>\
 						<tr>\
 							<td align='right'><b>Last increase time</b></td>\
-							<td></td>\
+							<td>{{describeTable.ProvisionedThroughput.LastIncreaseDateTime || '-'}}</td>\
 						</tr>\
 						<tr>\
-							<td align='right'><b>Storage size (in bytes)	</b></td>\
-							<td></td>\
+							<td align='right'><b>Storage size (in bytes)</b></td>\
+							<td>{{describeTable.TableSizeBytes }}</td>\
 						</tr>\
 						<tr>\
 							<td align='right'><b>Item count</b></td>\
-							<td></td>\
+							<td>{{ describeTable.ItemCount }}</td>\
 						</tr>\
 						<tr>\
 							<td align='right'><b>Region</b></td>\
@@ -354,7 +360,7 @@ Ractive.components.tableinfo = Ractive.extend({
 						</tr>\
 						<tr>\
 							<td align='right'><b>Amazon Resource Name (ARN)</b></td>\
-							<td></td>\
+							<td> {{describeTable.TableArn}}</td>\
 						</tr>\
 					</table>\
 					<small>Storage size and item count are not updated in real-time. They are updated periodically, roughly every six hours.</small>\
@@ -364,15 +370,32 @@ Ractive.components.tableinfo = Ractive.extend({
 	data: function() { return {} },
 	oninit: function() {
 		var ractive = this;
+		var refresh_table = function() {
+			ractive.set('describeTable', {})
+			routeCall({ method: 'describeTable', payload: { TableName: ractive.get('table.name')} }, function(err, data) {
+				if (err)
+					return ractive.set('err', err.errorMessage );
 
-		routeCall({ method: 'describeTable', payload: { TableName: ractive.get('table.name')} }, function(err, data) {
-			if (err)
-				return ractive.set('err', err.errorMessage );
+				console.log(data)
+				// data.Table.total_read =
+				// 	data.Table.ProvisionedThroughput.ReadCapacityUnits +
+				// 	(data.Table.GlobalSecondaryIndexes || []).map(function(gsi) { return gsi.ProvisionedThroughput.ReadCapacityUnits })
+				// 		.reduce(function (total, num) {return total + num;},0);
+				//
+				// data.Table.total_write =
+				// 	data.Table.ProvisionedThroughput.WriteCapacityUnits +
+				// 	(data.Table.GlobalSecondaryIndexes || []).map(function(gsi) { return gsi.ProvisionedThroughput.WriteCapacityUnits })
+				// 		.reduce(function (total, num) {return total + num;},0);
 
-			console.log(data)
-			ractive.set('describeTable', data.Table)
+				ractive.set('describeTable', data.Table)
+			})
+
+		}
+
+		ractive.on('refresh-table', function() {
+			refresh_table()
 		})
-
+		refresh_table()
 
 	},
 })
