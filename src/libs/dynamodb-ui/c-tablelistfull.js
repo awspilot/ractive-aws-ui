cbaea1b9e03ac6098a504de57c974b7a6748d9d2
adrianpraja <adrian@databank.ro>
2018-10-28 14:58:04 +0200

    update

---
+++ src/libs/dynamodb-ui/c-tablelistfull.js
@@ -0,0 +1,112 @@
+
+Ractive.components.tablelistfull = Ractive.extend({
+	//isolated: true,
+	template:
+		"\
+			<a class='btn btn-sm btn-default pull-right' on-click='@this.refresh_tables()'><i class='icon zmdi zmdi-refresh'></i></a>\
+			<a class='btn btn-sm btn-default pull-right' on-click='delete'><i class='icon zmdi zmdi-delete'></i></a>\
+			<a class='btn btn-sm btn-primary pull-right' on-click='create'><i class='icon zmdi zmdi-plus'></i> CREATE TABLE <i class='zmdi'></i></a>\
+		\
+		<tabledata columns='{{columns}}' rows='{{rows}}' style='top: 38px;'/>\
+		",
+	data: function() { return {} },
+	refresh_tables: function() {
+		var ractive = this
+
+		ractive.set('tables')
+
+		DynamoDB.explain().query('SHOW TABLES', function(err, call ) {
+			if (err)
+				return console.log(err)
+
+			routeCall( call, function( err, data ) {
+				if (err)
+					return ractive.set('err', err )
+
+				ractive.set('err')
+
+				ractive.set('columns', [ null, 'Name', 'Status', 'Partition', 'Sort', 'Indexes', 'Read Capacity', 'Write Capacity'])
+				ractive.set('rows', data.TableNames.map(function(t) {
+					return [
+						{ KEY: true },
+						{ S: t },
+						{ },
+						{ },
+						{ },
+						{ },
+						{ },
+						{ }
+					]
+				}) )
+				var waterfallz = data.TableNames.map(function(t) {
+
+					var f = function( cb ) {
+						//console.log(t)
+						routeCall({ method: 'describeTable', payload: { TableName: t} }, function(err, data) {
+							if (err)
+								return cb()
+
+							ractive.set('rows', ractive.get('rows').map(function(row) {
+								if ( row[1].S === t ) {
+
+									row[2].S = data.Table.TableStatus
+									row[3].S = ((data.Table.KeySchema || []).filter(function( ks ) { return ks.KeyType === 'HASH'})[0] || {}).AttributeName || '-'
+									row[4].S = ((data.Table.KeySchema || []).filter(function( ks ) { return ks.KeyType === 'RANGE'})[0] || {}).AttributeName || '-'
+									row[5].S = (data.Table.GlobalSecondaryIndexes || []).length.toString()
+									row[6].S = ([ data.Table.ProvisionedThroughput.ReadCapacityUnits ].concat( (data.Table.GlobalSecondaryIndexes || []).map(function(tr) { return tr.ProvisionedThroughput.ReadCapacityUnits }) )).reduce(function(a, b) { return a + b; }, 0)
+									row[7].S = ([ data.Table.ProvisionedThroughput.WriteCapacityUnits ].concat( (data.Table.GlobalSecondaryIndexes || []).map(function(tr) { return tr.ProvisionedThroughput.WriteCapacityUnits }) )).reduce(function(a, b) { return a + b; }, 0)
+
+								}
+								return row
+							}))
+							cb()
+						})
+					}
+					return f;
+				})
+
+				async.parallel(waterfallz, function( err ) {
+
+
+				})
+				//ractive.set('tables', data.TableNames )
+			} )
+		})
+		//ddb.listTables({}, function(err, data) {
+		//})
+	},
+	oninit: function() {
+		this.refresh_tables()
+		var ractive = this
+		//ractive.on('open-table', function(e, table ) {
+		//	ractive.root.fire('open-table', table )
+		//})
+		ractive.on('tabledata.selectrow', function(context) {
+			var keypath = context.resolve()
+			ractive.set(keypath + '.0.selected', !ractive.get(keypath + '.0.selected') )
+		})
+		ractive.on('delete', function() {
+			var selected = ractive.get('rows').filter(function(r) { return r[0].selected === true } );
+
+			if ( selected.length === 0 )
+				return alert('Please select a table to delete')
+
+			if ( selected.length > 1 )
+				return alert('Please select one table at a time')
+
+			var tablename = selected[0][1].S
+
+			if (confirm('Are you sure you want to delete table ' + tablename )) {
+				routeCall({ method: 'deleteTable', payload: { TableName: tablename } }, function(err, data) {
+					if (err)
+						return alert( err.errorMessage )
+					ractive.refresh_tables()
+				})
+			}
+
+		})
+		ractive.on('create', function() {
+			ractive.root.findComponent('tabs').newtab('tablecreate', 'Create Table' )
+		})
+	},
+})
