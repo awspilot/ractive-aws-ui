9fa845fb2f409762810eb4ef0ab0358b4ecbd88b
adrianpraja <adrian@databank.ro>
2018-10-28 19:27:47 +0200

    update

---
+++ src/libs/dynamodb-ui/c-dynamodb-ui.js
@@ -0,0 +1,39 @@
+var ddb;
+var DynamoSQL;
+var DynamoDB;
+var DynamodbFactory;
+Ractive.components.dynamoui = Ractive.extend({
+	template:
+		"\
+			<WindowHost />\
+			<header></header>\
+			<left>\
+				<minitablelist />\
+			</left>\
+			<content>\
+				<tabs active_id='tables' />\
+			</content>",
+	data: function() { return {} },
+	components: {
+		Window: RactiveWindow.default.Window,
+		WindowHost: RactiveWindow.default.WindowHost,
+	},
+	oninit: function() {
+		var credentials = this.get('account.key')
+
+		if (this.get('account.endpoint')) {
+			credentials.endpoint = this.get('account.endpoint')
+			if (this.get('account.endpoint').indexOf( location.protocol + '//' + location.host ) === 0) {
+				// dynamodb is proxied via same host, force version signature 3 so Authorization header is not used
+				credentials.signatureVersion = 'v3'
+				// httpOptions: { xhrWithCredentials: true },
+			}
+		}
+
+		ddb = new AWS.DynamoDB(credentials)
+		DynamoSQL = new window['@awspilot/dynamodb-sql'](ddb)
+		DynamodbFactory = window['@awspilot/dynamodb']
+		DynamoDB  = new DynamodbFactory(ddb)
+
+	},
+})
