279cb0a9455b6937da662dc7f76b74372d217a13
adrianpraja <adrian@databank.ro>
2018-11-25 12:01:24 +0200

    +createItem component

---
+++ src/libs/dynamodb-ui/win-item-create.js
@@ -0,0 +1,16 @@
+Ractive.components.CreateItem = Ractive.extend({
+	//isolated: true,
+	template:
+		"CreateItem not implemented",
+	data: function() {
+		return {
+
+		}
+	},
+
+	oninit: function() {
+		var ractive = this
+
+		console.log("createItem", ractive.get() )
+	},
+})
