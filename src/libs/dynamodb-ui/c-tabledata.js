f9d6910ba4394e7ad2a1d73379a395723c8eacb5
adrianpraja <adrian@databank.ro>
2018-12-02 14:18:54 +0200

    support for cellclick in table

--- src/libs/dynamodb-ui/c-tabledata.js
+++ src/libs/dynamodb-ui/c-tabledata.js
@@ -14,6 +14,7 @@ Ractive.components.tabledata = Ractive.extend({
 					{{#each .:i}}\
 					<div class='tabledatacell\
 						{{#if .KEY}}t-K{{/if}}\
+						{{#if .HASH}}t-HASH{{/if}}\
 						{{#if .S}}t-S{{/if}}\
 						{{#if .N}}t-N{{/if}}\
 						{{#if .BOOL}}t-BOOL{{/if}}\
@@ -21,7 +22,9 @@ Ractive.components.tabledata = Ractive.extend({
 						{{#if .L}}t-L{{/if}}\
 						{{#if .M}}t-M{{/if}}\
 						{{#if .U}}t-U{{/if}}\
-						' style='width: {{#if i === 0}}22px{{else}}{{100/columns.length}}%{{/if}} '>\
+						' style='width: {{#if i === 0}}22px{{else}}{{100/columns.length}}%{{/if}} '\
+						{{#if .HASH}}on-click='cellclick'{{/if}}\
+						>\
 						{{#if .KEY}}\
 							{{#if .selected}}\
 								<i class='zmdi selectrow zmdi-hc-fw zmdi-check-square'></i>\
@@ -29,6 +32,7 @@ Ractive.components.tabledata = Ractive.extend({
 								<i class='zmdi selectrow zmdi-hc-fw zmdi-square-o'></i>\
 							{{/if}}\
 						{{/if}}\
+						{{#if .HASH}}<a>{{.HASH}}</a>{{/if}}\
 						{{#if .S}}{{.S}}{{/if}}\
 						{{#if .N}}{{.N}}{{else}}{{#if .N === 0}}0{{/if}}{{/if}}\
 						{{#if .BOOL}}{{.BOOL}}{{/if}}\
@@ -44,5 +48,9 @@ Ractive.components.tabledata = Ractive.extend({
 		",
 	data: function() { return {} },
 	oninit: function() {
+		this.on('cellclick', function( e ) {
+			var col = this.get( e.resolve() )
+			this.fire('colclick', undefined, col.item )
+		})
 	}
 })
