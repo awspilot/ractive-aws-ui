b7bd73f01d5ebd226fe7f566777bc895cf890c9c
adrianpraja <adrian@databank.ro>
2019-02-09 12:42:25 +0200

    cf

---
+++ src/js/cloudformation/cloudformationui.js
@@ -0,0 +1,11 @@
+;
+Ractive.components.cloudformationui = Ractive.extend({
+	template:
+		`
+		this is clopudformation...
+	`,
+	oninit: function() {
+		console.log("cloudformation ui loading")
+	}
+})
+;
