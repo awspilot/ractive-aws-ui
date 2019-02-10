8190f2150e48f7c5731a166046aad1437459b77c
adrianpraja <adrian@databank.ro>
2019-02-10 22:27:25 +0200

    update

--- src/js/cloudformation/c-stacklist.js
+++ src/js/cloudformation/c-stacklist.js
@@ -3,7 +3,7 @@ Ractive.components.stacklist = Ractive.extend({
 	template: `
 		<div class="pull-right" style="padding: 7px;">
 			<a class="btn btn-xs btn-primary" on-click="create-stack"><i class="icon zmdi zmdi-plus"></i> CREATE STACK </a>
-			<a class="btn btn-xs btn-default disabled"><i class="icon zmdi zmdi-delete"></i></a>
+			<a class="btn btn-xs btn-default {{#if selection_length > 0}}{{else}}disabled{{/if}}" on-click='delete' as-tooltip=' \" Delete stack \" '><i class="icon zmdi zmdi-delete"></i></a>
 			<a class="btn btn-xs btn-default" on-click="refresh"><i class="icon zmdi zmdi-refresh"></i></a>
 		</div>
 
@@ -21,8 +21,6 @@ Ractive.components.stacklist = Ractive.extend({
 			//    /* more items */
 			//  ]
 		}, function(err, data) {
-
-			console.log("ListStacks",err,data)
 
 			if (err) {
 				if (cb) cb(err)
@@ -49,12 +47,41 @@ Ractive.components.stacklist = Ractive.extend({
 		var ractive=this;
 		ractive.set('columns', [ null, 'Stack Name', 'Status', 'Created time'])
 		ractive.set('rows', [] )
+
+		ractive.on('tabledata.selectrow', function(context) {
+			var keypath = context.resolve()
+			ractive.set(keypath + '.0.selected', !ractive.get(keypath + '.0.selected') )
+
+			ractive.set('selection_length',
+				ractive.get('rows').filter(function(r) { return r[0].selected === true } ).length
+			)
+		})
+
 		ractive.on('refresh', function() {
 			ractive.stack_list()
 		})
 
 		ractive.on('create-stack', function() {
 			ractive.root.findComponent('cftabs').newtab('stackcreate', 'Create Stack' )
+		})
+		ractive.on('delete', function() {
+			var selected = ractive.get('rows').filter(function(r) { return r[0].selected === true } );
+
+			if ( selected.length === 0 )
+				return alert('Please select a stack to delete')
+
+			if ( selected.length > 1 )
+				return alert('Please select one stack at a time')
+
+			var stackname = selected[0][1].S
+
+			if (confirm('Are you sure you want to delete stack ' + stackname )) {
+
+				cloudformation.deleteStack({ StackName: stackname, }, function(err, data) {
+
+				});
+			}
+
 		})
 		ractive.stack_list()
 	},
