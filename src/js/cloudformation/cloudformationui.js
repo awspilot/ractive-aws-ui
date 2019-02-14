8f4ee4f64fc7584553b4332cf84dcda807fddd95
adrianpraja <adrian@databank.ro>
2019-02-14 17:35:03 +0200

    update

--- src/js/cloudformation/cloudformationui.js
+++ src/js/cloudformation/cloudformationui.js
@@ -4,11 +4,15 @@ var cloudformation = null;
 Ractive.components.cloudformationui = Ractive.extend({
 	template:
 		`
-			<hsplit style='top: 56px;'>
-				<left>
-					<div style="display: block;height: 28px;line-height: 28px;font-size: 12px;padding-left: 10px;background: linear-gradient(#eee, #e0e0e0);border: 1px solid #b9b8b6;border-top: 1px solid #fff;border-bottom: 1px solid #ccc;">Cloudformation</div>
-					<div style="position: absolute;bottom: 0px;top: 29px;left: 0px;right: 0px;border: 1px solid #b9b8b6;">
-						<div style="display: block;height: 30px;line-height: 30px;font-size: 13px;padding: 0px 10px;border-top: 1px solid transparent;border-left: 1px solid transparent;margin-bottom: 0px;cursor: pointer;border-bottom: 1px solid #e0e0e0;color: #146eb4;">Stacks</div>
+			<hsplit style='top: 41px;'>
+				<left style="border: 1px solid #b9b8b6;">
+					<div style="display: block;font-size: 12px;padding-left: 10px;font-size: 18px;font-weight: 700;color: #000;line-height: 2rem;padding: 12px 35px;border-bottom: 1px solid #ddd;">
+						Cloudformation
+					</div>
+					<div style="position: absolute;bottom: 0px;top: 60px;left: 0px;right: 0px;">
+						<div style="display: block;height: 30px;line-height: 30px;font-size: 13px;padding: 0px 35px;border-top: 1px solid transparent;border-left: 1px solid transparent;margin-bottom: 0px;color: #ec7211;font-weight: bold;">Stacks</div>
+						<div style="display: block;height: 30px;line-height: 30px;font-size: 13px;padding: 0px 35px;border-top: 1px solid transparent;border-left: 1px solid transparent;margin-bottom: 0px;color: #000;">StacksSets</div>
+						<div style="display: block;height: 30px;line-height: 30px;font-size: 13px;padding: 0px 35px;border-top: 1px solid transparent;border-left: 1px solid transparent;margin-bottom: 0px;color: #000;">Exports</div>
 					</div>
 				</left>
 				<content>
