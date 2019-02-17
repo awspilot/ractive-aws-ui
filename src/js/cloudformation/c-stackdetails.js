da4195af3d80d72f026e6a15e1573b710b3bbfd5
adrianpraja <adrian@databank.ro>
2019-02-17 12:12:52 +0200

    show parameters for stack

--- src/js/cloudformation/c-stackdetails.js
+++ src/js/cloudformation/c-stackdetails.js
@@ -100,8 +100,41 @@ Ractive.components.stackdetailstemplate = Ractive.extend({
 ;
 Ractive.components.stackdetailsparameters = Ractive.extend({
 	template: `
-		Not Implemented
+		<tabledata columns='{{columns}}' rows='{{rows}}' style='top: 10px;' />
 	`,
+	oninit: function() {
+		var ractive=this;
+
+		ractive.set('columns', [ '', 'Key', 'Value', 'Resolved Value'])
+		ractive.set('rows', [] )
+
+		var params = {
+			StackName: this.get('StackName'),
+			//TemplateStage: Original | Processed
+		};
+		cloudformation.describeStacks(params, function(err, data) {
+			if (err)
+				return alert('failed describing stack')
+
+			console.log(data.Stacks[0])
+
+
+			ractive.set('stack',data.Stacks[0])
+
+			ractive.set('rows',
+				data.Stacks[0].Parameters.map(function(p) {
+					return [
+						{ S: '' },
+						{ S: p.ParameterKey },
+						{ S: p.ParameterValue },
+						{ S: '' },
+					]
+				})
+			)
+
+		});
+
+	}
 });
 Ractive.components.stackdetailsresources = Ractive.extend({
 	template: `
