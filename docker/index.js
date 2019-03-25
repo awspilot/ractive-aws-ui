29de156a8239da50d70acd6a2e68f80fb5e04ff4
adrianpraja <adrian@databank.ro>
2019-03-25 18:39:04 +0200

    update

--- docker/index.js
+++ docker/index.js
@@ -8,4 +8,5 @@ new RoutingProxy({ agent: false })
 	.upstream('/v1/cloudformation', [ 'http://localhost:10001' ])
 	.upstream('/v1/dynamodb', [ 'http://localhost:8000' ])
 	.upstream('/v1/wrapdynamodb', [ 'http://localhost:10002' ])
+	.upstream('/v1/s3', [ 'http://localhost:10003' ])
 	.listen(80);
