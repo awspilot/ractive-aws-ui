b9d39c201a822717a02581baca5ac836290873fe
databank <adrian@databank.ro>
2019-03-26 13:58:09 +0200

    update

--- docker/index.js
+++ docker/index.js
@@ -1,12 +1,12 @@
 
 
 
-var RoutingProxy = require('./proxylib');
+var RoutingProxy = require('@awspilot/cloudfront-mock');
 
 new RoutingProxy({ agent: false })
-	.upstream('/', 'http://localhost:10000')
-	.upstream('/v1/cloudformation', [ 'http://localhost:10001' ])
-	.upstream('/v1/dynamodb', [ 'http://localhost:8000' ])
-	.upstream('/v1/wrapdynamodb', [ 'http://localhost:10002' ])
-	.upstream('/v1/s3', [ 'http://localhost:10003' ])
+	.upstream('/', 'http://localhost:10000/')
+	.upstream('/v1/cloudformation', [ 'http://localhost:10001/v1/cloudformation' ])
+	.upstream('/v1/dynamodb', [ 'http://localhost:8000/v1/dynamodb' ])
+	.upstream('/v1/wrapdynamodb', [ 'http://localhost:10002/v1/wrapdynamodb' ])
+	.upstream('/v1/s3', [ 'http://localhost:10003/v1/s3' ])
 	.listen(80);
