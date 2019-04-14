062f600491345ee21bca2157b52e3b444cc499cc
databank <adrian@databank.ro>
2019-04-14 13:37:37 +0300

    update

--- src/js/dynamodb/c-dynamodb-ui.js
+++ src/js/dynamodb/c-dynamodb-ui.js
@@ -3,20 +3,6 @@ var DynamoDB;
 var DynamodbFactory;
 
 
-/*
-var params = {
-  BackupType: USER | SYSTEM | AWS_BACKUP | ALL,
-  ExclusiveStartBackupArn: 'STRING_VALUE',
-  Limit: 'NUMBER_VALUE',
-  TableName: 'STRING_VALUE',
-  TimeRangeLowerBound: new Date || 'Wed Dec 31 1969 16:00:00 GMT-0800 (PST)' || 123456789,
-  TimeRangeUpperBound: new Date || 'Wed Dec 31 1969 16:00:00 GMT-0800 (PST)' || 123456789
-};
-dynamodb.listBackups(params, function(err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else     console.log(data);           // successful response
-});
-*/
 
 
 Ractive.components.dynamoui = Ractive.extend({
