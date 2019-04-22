4d61cd5c696ba6bbb5bc329c1ef32dcf6e37a0d9
databank <adrian@databank.ro>
2019-04-22 10:24:40 +0300

    update

--- src/libs/ractive/ractive-chart.js
+++ src/libs/ractive/ractive-chart.js
@@ -6,34 +6,47 @@ Ractive.components.chart = Ractive.extend({
 	
 	template: `
 		<div id="{{id}}" class="{{class}}" style="{{style}};display: inline-block;" on-resize="resize">
-		
+			<style>
+				.bar:hover {
+					fill: darkblue;
+				}
+				.tooltip {
+					background: cornsilk;
+					border: 1px solid black;
+					border-radius: 5px;
+					padding: 5px;
+				}
+			</style>
 
 			{{#if width && height }}
-
+				<div class="tooltip" style="position: absolute; display: none;"></div>
+				
 				<svg width="{{width}}" height="{{height}}" viewBox="0 0 {{width}} {{height}}">
+
 
 					<rect x="0" y="0" width="{{width}}" height="{{height}}" fill="#f0f0f0"></rect>
 					<rect x="{{marginLeft}}" y="0" width="{{ width - marginLeft }}" height="{{ height - marginBottom }}" fill="#f0f0f0"></rect>
 
+
 					<g class="grid">
 						<!-- x axis -->
-						<line x1="{{marginLeft + (70 * 0) }}" y1="{{ marginTop }}" x2="{{marginLeft + (70 * 0) }}" y2="{{ height - marginBottom }}" fill="none" shape-rendering="crispEdges" stroke="#ccc" stroke-dasharray="5,2" stroke-width="1"></line>
-						<line x1="{{marginLeft + (70 * 1) }}" y1="{{ marginTop }}" x2="{{marginLeft + (70 * 1) }}" y2="{{ height - marginBottom }}" fill="none" shape-rendering="crispEdges" stroke="#ccc" stroke-dasharray="5,2" stroke-width="1"></line>
-						<line x1="{{marginLeft + (70 * 2) }}" y1="{{ marginTop }}" x2="{{marginLeft + (70 * 2) }}" y2="{{ height - marginBottom }}" fill="none" shape-rendering="crispEdges" stroke="#ccc" stroke-dasharray="5,2" stroke-width="1"></line>
-						<line x1="{{marginLeft + (70 * 3) }}" y1="{{ marginTop }}" x2="{{marginLeft + (70 * 3) }}" y2="{{ height - marginBottom }}" fill="none" shape-rendering="crispEdges" stroke="#ccc" stroke-dasharray="5,2" stroke-width="1"></line>
+						<line x1="{{marginLeft + (70 * 0) }}" y1="{{ marginTop +2 }}" x2="{{marginLeft + (70 * 0) }}" y2="{{ height - marginBottom }}" fill="none" shape-rendering="crispEdges" stroke="#ccc" stroke-dasharray="5,2" stroke-width="1"></line>
+						<line x1="{{marginLeft + (70 * 1) }}" y1="{{ marginTop +2 }}" x2="{{marginLeft + (70 * 1) }}" y2="{{ height - marginBottom }}" fill="none" shape-rendering="crispEdges" stroke="#ccc" stroke-dasharray="5,2" stroke-width="1"></line>
+						<line x1="{{marginLeft + (70 * 2) }}" y1="{{ marginTop +2 }}" x2="{{marginLeft + (70 * 2) }}" y2="{{ height - marginBottom }}" fill="none" shape-rendering="crispEdges" stroke="#ccc" stroke-dasharray="5,2" stroke-width="1"></line>
+						<line x1="{{marginLeft + (70 * 3) }}" y1="{{ marginTop +2 }}" x2="{{marginLeft + (70 * 3) }}" y2="{{ height - marginBottom }}" fill="none" shape-rendering="crispEdges" stroke="#ccc" stroke-dasharray="5,2" stroke-width="1"></line>
 
 
 
 						<!-- y axis -->
-						<line x1="{{marginLeft}}" y1="{{height - marginBottom - (70 * 1 ) }}" x2="{{width - marginRight }}" y2="{{height - marginBottom - (70 * 1 ) }}" fill="none" shape-rendering="crispEdges" stroke="#ccc" stroke-dasharray="5,2" stroke-width="1"></line>
-						<line x1="{{marginLeft}}" y1="{{height - marginBottom - (70 * 2 ) }}" x2="{{width - marginRight }}" y2="{{height - marginBottom - (70 * 2 ) }}" fill="none" shape-rendering="crispEdges" stroke="#ccc" stroke-dasharray="5,2" stroke-width="1"></line>
-
-
+						{{#each Array( 6 ):i }}
+							<line x1="{{marginLeft}}" y1="{{height - marginBottom - (yAxis_tickInterval() * i ) }}" x2="{{width - marginRight }}" y2="{{height - marginBottom - (yAxis_tickInterval() * i ) }}" fill="none" shape-rendering="crispEdges" stroke="#ccc" stroke-dasharray="5,2" stroke-width="1"></line>
+						{{/each}}
 
 					</g>
 
+
 					<g class="axes">
-						<polyline points="{{marginLeft}},{{marginTop}} {{marginLeft}},{{height - marginBottom}} " fill="none" shape-rendering="crispEdges" stroke="#bbb" stroke-width="1"></polyline>
+						<polyline points="{{marginLeft}},{{marginTop +2 }} {{marginLeft}},{{height - marginBottom}} " fill="none" shape-rendering="crispEdges" stroke="#bbb" stroke-width="1"></polyline>
 						<polyline points="{{marginLeft}},{{height - marginBottom}} {{width - marginRight}}, {{height - marginBottom}}" fill="none" shape-rendering="crispEdges" stroke="#bbb" stroke-width="1"></polyline>
 					</g>
 
@@ -42,7 +55,7 @@ Ractive.components.chart = Ractive.extend({
 					{{/if}}
 					{{#if series.length}}
 						{{#series.0.data:i}}
-							<rect class="bar" fill="#7cb5ec" x="{{ ~/marginLeft + ( (~/bar_width() || 10) * i ) + ((i+1)*1)  }}" width="{{ ~/bar_width() }}" y="{{ ~/height - ~/marginBottom - ~/bar_height( . ) -1 }}" height="{{ ~/bar_height( . ) }}"></rect>
+							<rect class="bar" fill="#7cb5ec" x="{{ ~/marginLeft + ( (~/bar_width() || 10) * i ) + ((i+1)*1)  }}" width="{{ ~/bar_width() }}" y="{{ ~/height - ~/marginBottom - ( ~/pixels_per_unit() * .[1] ) -1 }}" height="{{ ~/pixels_per_unit() * .[1] }}" on-mousemove="showTooltip" on-onmouseout="hideTooltip"></rect>
 						{{/}}
 					{{/if}}
 
@@ -106,16 +119,25 @@ Ractive.components.chart = Ractive.extend({
 			bar_width: function( value ) {
 				return ((this.get('width') - this.get('marginLeft') - this.get('marginRight') ) / (((this.get('series') || {})[0] || {}).data || []).length)-1;
 			},
-			bar_height: function( value ) {
-
+			canvas_height: function() {
+				var canvas_height = ((this.get('height') - this.get('marginBottom') - this.get('marginTop') )* 100)/100    - 2;
+				//if (this.get('debug')) console.log("canvas_height", canvas_height )
+				return  canvas_height; // biggest should go to up-to 100
+			},
+			max_value: function() {
 				var series = this.get('series');
 				var max = 0;
 				((series[0] || {}).data || []).map(function(d) {
 					max = Math.max(d[1], max)
-				})
+				});
+				//if (this.get('debug')) console.log("max_value", max )
+				return max;
+			},
+			bar_height: function( value ) {
 
-				var canvas_height = ((this.get('height') - this.get('marginBottom') - this.get('marginTop') )* 80)/100    - 2; // biggest should go to up-to 100
+				var max = this.get('max_value')()
 
+				var canvas_height = this.get('canvas_height')()
 
 				var bar_height = (value[1] * canvas_height)/max
 				
@@ -124,7 +146,73 @@ Ractive.components.chart = Ractive.extend({
 				return bar_height;
 			},
 			
+			pixels_per_unit: function() {
+
+				var canvas_height = this.get('canvas_height')()
+				var limits_arr = Array(5).fill().map(function(k, i) { return Math.pow(10, i) })
+								.concat(Array(5).fill().map(function(k, i) { return 2 * (( Math.pow(10, i) ) || 1) }))
+								.concat(Array(5).fill().map(function(k, i) { return 5 * (( Math.pow(10, i) ) || 1) }))
+								.sort(function(a,b) { return a > b ? -1 : 1})
+								.filter(function(v) { return v > 4});
+
+				var max = this.get('max_value')()
+
+				var top_limit = limits_arr[0];
+				limits_arr.map(function(limit) { if ( max < limit ) top_limit = limit; })
+
+				//console.log("top_limit=", top_limit )
+
+				var pixels_per_unit = canvas_height / top_limit
+				//console.log("pixels_per_unit=", pixels_per_unit )
+
+				return pixels_per_unit
+			},
 			
+			yAxis_tickInterval: function() {
+				// if (this.get('yAxis.tickInterval'))
+				// 	return this.get('yAxis.tickInterval');
+				
+
+				var canvas_height = this.get('canvas_height')()
+
+				// console.log("top_limit=", top_limit )
+				// var number_of_bars = 6;
+				// var units_per_bar = top_limit / 5;
+
+
+				var limits_arr = Array(5).fill().map(function(k, i) { return Math.pow(10, i) })
+								.concat(Array(5).fill().map(function(k, i) { return 2 * (( Math.pow(10, i) ) || 1) }))
+								.concat(Array(5).fill().map(function(k, i) { return 5 * (( Math.pow(10, i) ) || 1) }))
+								.sort(function(a,b) { return a > b ? -1 : 1})
+								.filter(function(v) { return v > 4});
+
+				var series = this.get('series');
+				var max = 0;
+				((series[0] || {}).data || []).map(function(d) {
+					max = Math.max(d[1], max)
+				})
+
+				//console.log("max=", max )
+				
+				var top_limit = limits_arr[0];
+				limits_arr.map(function(limit) { if ( max < limit ) top_limit = limit; })
+				
+				//console.log("top_limit=", top_limit )
+
+				var pixels_per_unit = canvas_height / top_limit
+				//console.log("pixels_per_unit=", pixels_per_unit )
+
+				return canvas_height / 5;
+
+
+			},
+			
+			yAxis: {
+				//tickInterval: 10,
+			},
+			yTicks: function() {
+				return 5;
+			},
 			// defaults
 			marginTop: 20,
 			marginLeft: 20,
@@ -137,8 +225,23 @@ Ractive.components.chart = Ractive.extend({
 		ractive.on('resize', function(e) {
 			ractive.set('width', e.width)
 			ractive.set('height', e.height)
-			console.log('resize', e.width, e.height )
+			
+
+			//console.log('resize', e.width, e.height )
 		})
+
+		ractive.on('showTooltip', function(e) {
+			// let tooltip = e.node.parentNode.parentNode.getElementsByClassName('tooltip')[0]
+			// tooltip.innerHTML = 'this is tooltip';
+			// tooltip.style.display = "block";
+			// tooltip.style.left = e.original.offsetX + 10 + 'px';
+			// tooltip.style.top = e.original.offsetY + 10 + 'px';
+		})
+		ractive.on('hideTooltip', function(e) {
+			// let tooltip = e.node.parentNode.parentNode.getElementsByClassName('tooltip')[0]
+			// tooltip.style.display = "none";
+		})
+
 	}
 })
 ;
